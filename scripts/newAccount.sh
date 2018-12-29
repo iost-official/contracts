1502b6c8939277418d6fcb477ee2c5285ab0dbbc
He Peijian <glisxx6@gmail.com>
2018-12-29 15:44:33 +0800

    more pledge of new account

--- scripts/newAccount.sh
+++ scripts/newAccount.sh
@@ -1,14 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 
-if [[ ! -f "~/.wallet/admin_ed25519" ]]; then
+if [[ -f "~/.wallet/admin_ed25519" ]]; then
   iwallet account --import admin 2yquS3ySrGWPEKywCPzX4RTJugqRh7kJSo5aehsLYPEWkUxBWA39oMrZ7ZxuM4fgyXYs2cPwh5n8aNNpH5x2VyK1
 fi
 
 iwallet \
-  account --create $1 \   # create a account named player0, NOTICE: duplicated name will failed
-  --initial_ram 4096  \  # ram in byte
-  --initial_gas_pledge 10000 \
+  account --create $1 \
+  --initial_ram 65536  \
+  --initial_gas_pledge 1000000 \
   --initial_balance 10000 \
   --amount_limit '*:unlimited' \
-  --account admin \       # set account to publish new account transaction
+  --account admin \
   --server 47.244.109.92:30002
